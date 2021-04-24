b82dc7cb7ec0f1957bc8bdd75ed665da0d47323d
rilac1 <rilac1@naver.com>
2021-04-24 18:13:55 +0900

    kang

--- manual.py
+++ manual.py
@@ -8,21 +8,22 @@ from rr         import RR
 from mlfq       import MLFQ_1
 from mlfq       import MLFQ_2
 
-def HELP():
-    usage = """
+usage = """
     Usage : scheduler.py [--help] or scheduler.py [-h]
+            scheduler.py
             scheduler.py [--sjf]
             scheduler.py [--fcfs]
             scheduler.py [--lott]
             scheduler.py [--rr] [-q num]
-            scheduler.py [--mlfq] [-q num]
+            scheduler.py [--mlfq] [-q num] """
 
+supplement = """
             -- sjf: Shortest Job First
             -- fcfs: First Come First Served
             -- lott: Lottery
             -- rr: Round Robin
             -- mlfq: Multi Level Feedback Queue
-            -q: tume quantum
+            -q: time quantum
 
     *  If you choose RR or MLFQ, You must set the time quantum.
     *  At MLFQ, if you set a time quantum as a negative number (ex. -n),
@@ -33,12 +34,17 @@ def HELP():
 
     *  If you want to run it as a user interface, 
       you don\'t need to enter any arguments.
+    
+    'This program was developed in Python version %s'
+    """ %sys.version
 
-    """
+def HELP():
     print(usage)
-    print('This program was developed in Python version ' + sys.version)
+    print(supplement)
     exit()
 
+def USAGE():
+    print(usage)
 
 def INTERFACE():
     op = 0
@@ -62,16 +68,12 @@ def INTERFACE():
         print('Set the time quantum: ', end = '')
         q = int(input())
         RR(q).simulate()
-    elif op == 4:
+    elif op == 4:        
         print('[If you want to set the time quantum according to the level, \n'
         + 'input the time quantum as negative a number]')
         print('Set the time quantum', end = '')
         q = int(input())
         if q>0:
             MLFQ_1(q).simulate()
-        else:
-            MLFQ_2(q).simulate()
     else:
-        HELP()
-
-    exit()
+        print('Error! Input a number from 1 to 5')
--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,6 @@
 import sys
 import getopt
-import usage
+import manual
 from readyqueue import ReadyQueue
 from initialize import Initialize
 from fcfs       import FCFS
@@ -12,35 +12,45 @@ from mlfq       import MLFQ_2
 def main():
     rr_or_mlfq = 0      # RR: 1, MLFQ: 2
     q = 0
-    options, args = getopt.getopt(sys.argv[1:], 'hq:', 
-    ['help', 'sjf', 'fcfs','rr','mlfq','lott'])
+
+    try:
+        options, args = getopt.getopt(sys.argv[1:], 'hq:', 
+        ['help', 'sjf', 'fcfs','rr','mlfq','lott'])
+    
+    except:
+        manual.USAGE()
+        exit(2)
 
     if len(options) == 0:
-        usage.INTERFACE()
+        manual.INTERFACE()
 
     for op in options[0]:
         if op in ("-h", "--help"):
-            usage.HELP()
+            manual.HELP()
+            exit()
         elif op == '--sjf':
             SJF().simulate()
+            exit()
         elif op == '--fcfs':
             FCFS().simulate()
+            exit()
         elif op == '--rr':
             rr_or_mlfq = 1
         elif op == '--mlfq':
             rr_or_mlfq = 2
         elif op == '--lott':
             LOTT().simulate()
+            exit()
         elif op == '-q':
             q = op[1]
             if rr_or_mlfq==1:
                 RR(q).simulate()
+                exit()
             elif rr_or_mlfq==2:
-                MLFQ(q).simulate()           
-        else:
-            continue
+                MLFQ(q).simulate()
+                exit()
     if rr_or_mlfq and q==0:
-        print('RR or MLFQ, You must set the time quantum.')
+        print('Error! RR or MLFQ, You must set the time quantum.')
         print('more: scheduler.py [-help] or scheduler.py [-h]')
 
 
