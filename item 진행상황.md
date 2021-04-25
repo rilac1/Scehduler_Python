1d0c41a53faca557dd80fc5d0a36623cb5ad7bdf
rilac1 <rilac1@naver.com>
2021-04-26 01:21:31 +0900

    kang

--- item 진행상황.md
+++ item 진행상황.md
@@ -3,7 +3,7 @@
 2 - fcfs  
 3 - initalize  
 4 - initalize  
-<br>
+5 - scheduler
 6 - scheduler  
 7 - initialize  
 <br>
@@ -14,11 +14,11 @@
 20 - readyqueue  
 underscore - initalize  
 <br>
-*총 11개 완료함*
+*총 12개 완료함*
 
+---
 ### * 미완
-> 5 - 복잡한 식 대신 `Helper` 함수 사용해라.  
-8 - 여러개의 리스트에서 동일한 인덱스를 사용할 때 `zip`을 사용해라.  
+> 8 - 여러개의 리스트에서 동일한 인덱스를 사용할 때 `zip`을 사용해라.  
 12 - 슬라이싱과 인덱싱을 한번에 하지 마라.  
 13 - 슬라이싱 보다는 언패킹을 사용해라.  
 <br>
@@ -29,4 +29,5 @@ underscore - initalize
 16 - 딕셔너리 키가 없을 때 처리는 `get`을 이용해라.  
 17 - `defaultdick` 를 사용해서 키가 없는 경우를 처리해라.  
 18 - `__missing__`를 사용해서 키에 따라 다른 디폴트 값을 생성해라.  
+<br>
 19 - 함수의 반환값이 3개 이상이면 안된다.  
--- scheduler.py
+++ scheduler.py
@@ -27,8 +27,13 @@ def main():
 
     if len(options) == 0:
         manual.INTERFACE()
+    # [Item 5] Write Helper Functions Instead of Complex Expressions
+    helper(options)
+    # 복잡한 식 대신에 helper 함수를 따로 작성하여서 프로그램을 모듈화하였고 이를 통해 main 함수를 간결하게 하였습니다.
 
+def helper(options):
     for op in options:
+        # [Item 10] Issue 3. Simulate switch-case statement
         if op[0] in ("-h", "--help"):
             manual.HELP()
             exit()
@@ -47,19 +52,19 @@ def main():
             exit()
         elif op[0] == '-q':
             q = int(op[1])
-            # [Item 10] Issue 3. Simulate switch-case statement
+        #> else - if 문을 elif 문으로 사용하여 가독성을 높였습니다.
             if rr_or_mlfq==1:
                 RR(q).simulate()
                 exit()
             elif rr_or_mlfq==2:
                 MLFQ(q).simulate()
                 exit()
-            #> else - if 문을 elif 문으로 사용하여 가독성을 높였습니다.
     # [Item9] Avoid else Block after for and
     else:
         print('Error! RR or MLFQ, You must set the time quantum.')
         print('more: scheduler.py [-help] or scheduler.py [-h]')
     #> for문을 모두 수행했다면 어떤 조건도 만족하지 못했다는 뜻이므로 에러처리를 해주기 위해 for-else문을 사용하였습니다.
+
 
 
 if __name__ == "__main__": 
